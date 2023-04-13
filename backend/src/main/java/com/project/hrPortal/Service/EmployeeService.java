18f7ea056b2dda7cad8085f8cd2a7c5bc11642c7
chinmayi sunku <chinmayi.sunku@sjsu.edu>
2023-04-13 13:40:35 -0700

    backend changed

--- backend/src/main/java/com/project/hrPortal/Service/EmployeeService.java
+++ backend/src/main/java/com/project/hrPortal/Service/EmployeeService.java
@@ -5,11 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface EmployeeService extends JpaRepository<Employees, Long> {
     public Employees findEmployeeByEmpNo (int empNo);
 
-//    @Query("select * from employees e, titles t where e.emp_no = t.emp_no")
-//    List<>
+    @Query("select * from employees e, titles t where e.emp_no = t.emp_no")
+    List<Object[]> getTitles();
 }
 
